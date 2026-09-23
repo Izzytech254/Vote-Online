fcfb031773472c9b1b603f109aa36b3b11179f20
Israel Kumala <israelnicodemus10@gmail.com>
2026-09-23 20:04:41 +0300

    Restyle countdown and drop milliseconds

--- script.js
+++ script.js
@@ -16,13 +16,11 @@ function updateCountdown() {
   const hours = Math.floor(diff / 3600000);
   const minutes = Math.floor(diff % 3600000 / 60000);
   const seconds = Math.floor(diff % 60000 / 1000);
-  const millis = Math.floor(diff % 1000);
   document.querySelector("#cd-hours").textContent = String(hours).padStart(2, "0");
   document.querySelector("#cd-minutes").textContent = String(minutes).padStart(2, "0");
   document.querySelector("#cd-seconds").textContent = String(seconds).padStart(2, "0");
-  document.querySelector("#cd-millis").textContent = String(millis).padStart(3, "0");
 }
-setInterval(updateCountdown, 37);
+setInterval(updateCountdown, 1000);
 updateCountdown();
 
 function renderCandidates() {
